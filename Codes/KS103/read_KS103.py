c4dd8f2a19c83829c48a4fbe402b0d45bd2b5aa7
Guasch5000 <guasch5000@gmail.com>
2014-03-18 12:57:18 +0100

    adding reading with two devices

--- Codes/KS103/read_KS103.py
+++ Codes/KS103/read_KS103.py
@@ -2,16 +2,23 @@ import smbus
 import time
 bus = smbus.SMBus(1)
 
+#KS103's new address
 address = 0x73
 
 try:
 	while (True):
+		#Inicialize the lecture in [mm], range [0-5200mm]
 		bus.write_byte_data(address, 2, 0xb0)
+
+		#Max waiting time
 		time.sleep(0.033)
-		data = bus.read_byte(address)
-		data2 = bus.read_byte(address)
-		time.sleep(0.100)
-		print data, data2, data*256 + data2
+
+		#Read register 2 and 3
+		data_high = bus.read_byte(address)
+		data_low = bus.read_byte(address)
+
+		#Show distance
+		print  data_high << 8 | data_low
 		time.sleep(1)
 
 except:
